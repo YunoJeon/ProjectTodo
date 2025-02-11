88b3791a513184df63bc5569325e1bbbd522f7c6
jeon-yuno <zero0577@naver.com>
2025-02-11 18:01:26 +0900

    feat: search 관련 - Todo 및 Project 검색을 위한 로직 구현

--- src/main/java/com/todo/project/repository/ProjectRepository.java
+++ src/main/java/com/todo/project/repository/ProjectRepository.java
@@ -2,6 +2,7 @@ package com.todo.project.repository;
 
 import com.todo.project.entity.Project;
 import com.todo.user.entity.User;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
   Page<Project> findByOwner(User owner, Pageable pageable);
+
+  List<Project> findByNameContainingIgnoreCase(String keyword);
 }
---
+++ src/main/java/com/todo/search/type/SearchType.java
@@ -0,0 +1,5 @@
+package com.todo.search.type;
+
+public enum SearchType {
+  TODO, PROJECT
+}
